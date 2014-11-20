b685aabb993e446901ef8d28d63ed56c1833591c
jrojascarrilo <rojascrc@hotmail.com>
2014-11-19 18:22:20 -0600

    Se anade la funcion de logueo

--- main.rb
+++ main.rb
@@ -2,13 +2,21 @@ require 'sinatra'
 require './usuarios.rb'
 
 get '/' do
-	"Hello, world!"
+	"Bienvenido a la aplicacion de Vuelvase loco entreprise!"
+end
+
+#Caso para ingresar un usuario nuevo
+get '/ingresar/:nombre/:pass' do
+	actual = Usuario.new(params[:nombre],params[:pass]) #Se crea un objeto con los datos del usuario
+	imprimir = actual.ingresarUsuario #Es el valor de respuesta a la aplicacion
+	"" + imprimir + ""
 end
 
 
-get '/ingresar/:nombre/:pass' do
-	actual = Usuario.new(params[:nombre],params[:pass])
-	imprimir = actual.ingresarUsuario
-	" " + imprimir + ""
+#Caso para loguearse 
+get '/loguear/:nombre/:pass' do 
+	actual = Usuario.new(params[:nombre],params[:pass]) #Se crea un objeto con los datos del usuario
+	imprimir = actual.loguear #Es el valor de respuesta a la aplicacion
+	"" + imprimir + ""
 end
 
